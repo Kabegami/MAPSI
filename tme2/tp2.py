7582cc0fa8f76c2ded99962efe19d2fc40e0f588
Kabegami <lucas.becirspahic@gmail.com>
2017-10-02 12:52:37 +0200

    2.4

--- tme2/tp2.py
+++ tme2/tp2.py
@@ -5,6 +5,8 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import pyAgrum as gum
+import pyAgrum.lib.ipython as gnb
 plt.close('all')
 
 def combinaison(k,n):
@@ -95,13 +97,7 @@ def histogramme_binomiale(n,p=0.5):
 def Pxy(x, y):
     """ float np.array x float np.array -> float np.2D-array 
     on fait le produit tensoriel et non le produit matriciel"""
-    L = []
-    for xi in x:
-        line = []
-        for yi in y:
-            line.append(xi * yi)
-        L.append(line)
-    return np.array(L)
+    return np.outer(x,y)
 
 def produit_tensoriel(A, B):
     """ A : matrice, B : matrice """
@@ -147,30 +143,174 @@ def dessine(P_jointe) :
     ax.set_ylabel('B')
     ax.set_zlabel('P(A) * P(B) ')
     plt.show()
-            
+
+def independance_conditionnelles():
+    P_XYZT = np.array([[[[ 0.0192,  0.1728],
+                     [ 0.0384,  0.0096]],
+
+                    [[ 0.0768,  0.0512],
+                     [ 0.016 ,  0.016 ]]],
+
+                   [[[ 0.0144,  0.1296],
+                     [ 0.0288,  0.0072]],
+
+                    [[ 0.2016,  0.1344],
+                     [ 0.042 ,  0.042 ]]]])
+
+    P_YZ = np.zeros((2,2))
+    for zi in range(len(P_XYZT)):
+        for yi in range(len(P_XYZT)):
+            s = 0
+            for xi in range(len(P_XYZT)):
+                for ti in range(len(P_XYZT)):
+                    s += P_XYZT[xi][yi][zi][ti]
+            P_YZ[yi][zi] = s
+    print(P_YZ)
+
+    P_XTcondYZ = np.zeros((2,2,2,2))
+    for xi in range(len(P_XYZT)):
+        for yi in range(len(P_XYZT)):
+            for zi in range(len(P_XYZT)):
+                for ti in range(len(P_XYZT)):
+                    P_XTcondYZ[xi][yi][zi][ti] = P_XYZT[xi][yi][zi][ti] / P_YZ[yi][zi]
+    print('P_XTcondYZ')
+    print(P_XTcondYZ)
+
+    print('P_XcondYZ')
+    P_XcondYZ = np.zeros((2,2,2))
+    for xi in range(len(P_XYZT)):
+        for yi in range(len(P_XYZT)):
+            for zi in range(len(P_XYZT)):
+                s = 0
+                for ti in range(len(P_XYZT)):
+                    s += P_XTcondYZ[xi][yi][zi][ti]
+                P_XcondYZ[xi][yi][zi] = s
+    print(P_XcondYZ)
+
+    print('P_TcondYZ')
+    P_TcondYZ = np.zeros((2,2,2))
+    for ti in range(len(P_XYZT)):
+        for yi in range(len(P_XYZT)):
+            for zi in range(len(P_XYZT)):
+                s = 0
+                for xi in range(len(P_XYZT)):
+                    s += P_XTcondYZ[xi][yi][zi][ti]
+                P_TcondYZ[ti][yi][zi] = s
+    print(P_TcondYZ)
+
+    print('Verif independance')
+    print(np.outer(P_XcondYZ, P_TcondYZ))
+
+    P_XYZ = np.zeros((2,2,2))
+    for xi in range(len(P_XYZT)):
+        for yi in range(len(P_XYZT)):
+            for zi in range(len(P_XYZT)):
+                s = 0
+                for ti in range(len(P_XYZT)):
+                    s += P_XYZT[xi][yi][zi][ti]
+                P_XYZ[xi][yi][zi] = s
+    print('P_XYZ')
+    print(P_XYZ)
+
+    P_X = np.zeros((2))
+    for xi in range(len(P_XYZ)):
+        s = 0
+        for yi in range(len(P_XYZ)):
+            for zi in range(len(P_XYZ)):
+                s += P_XYZ[xi][yi][zi]
+        P_X[xi] = s
+    print('Px')
+    print(P_X)
+
+    P_YZ = np.zeros((2,2))
+    for yi in range(len(P_XYZ)):
+        for zi in range(len(P_XYZ)):
+            s = 0
+            for xi in range(len(P_XYZ)):
+                s += P_XYZ[xi][yi][zi]
+            P_YZ[yi][zi] = s
+    print('P_YZ')
+    print(P_YZ)
+
+    print("P(X) * P(Y,Z)")
+    M = np.outer(P_X, P_YZ)
+    print(M)
+    #print(np.isclose(M,P_XYZ))
+
+def read_file ( filename ):
+    """
+    Renvoie les variables aléatoires et la probabilité contenues dans le
+    fichier dont le nom est passé en argument.
+    """
+    Pjointe = gum.Potential ()
+    variables = []
+
+    fic = open ( filename, 'r' )
+    # on rajoute les variables dans le potentiel
+    nb_vars = int ( fic.readline () )
+    for i in range ( nb_vars ):
+        name, domsize = fic.readline ().split ()
+        variable = gum.LabelizedVariable(name,name,int (domsize))
+        variables.append ( variable )
+        Pjointe.add(variable)
+
+    # on rajoute les valeurs de proba dans le potentiel
+    cpt = []
+    for line in fic:
+        cpt.append ( float(line) )
+    Pjointe.fillWith(np.array ( cpt ) )
+
+    fic.close ()
+    return np.array ( variables ), Pjointe
+
+def conditional_indep(potential, X, Y, Z, epsilon):
+    PXYZ = potential.margSumIn([x.name() for x in [X,Y] + Z])
+    P_XZ = PXYZ.margSumOut([Y.name()])
+    P_Z = PXYZ.margSumOut([Y.name(), X.name()])
+    P_XsachantZ = P_XZ / P_Z
+    P_YZ = PXYZ.margSumOut([X.name()])
+    P_YsachantZ = P_YZ / P_Z
+    Q = PXYZ - (P_XsachantZ * P_YsachantZ)
+    v = Q.toarray()
+    b = abs(v) > epsilon
+    return not(np.any(b))
+
+def compact_conditional_proba(potential, X, epsilon):
+    K = potential.margSumIn(X.name())
+    for xi in potential.variableSequence():
+        if conditional_indep(potential, xi, X, K , epsilon):
+            K -= xi
+    
+
+def gum_part():
+    LabelizedVariable, potential = read_file('asia.txt')
+    #print(LabelizedVariable)
+    #print(potential)
+    Q =conditional_indep(potential, LabelizedVariable[0], LabelizedVariable[1], [LabelizedVariable[2]], 0.1)
+    print(Q)
+    
+# -----------------------------------------------------
+#                 TEST
+# -----------------------------------------------------
+    
+def test_Pjointe():
+    PA = np.array ( [0.2, 0.7, 0.1] )
+    PB = np.array ( [0.4, 0.4, 0.2] )
+    Pjointe = Pxy(PA, PB)
+    dessine(Pjointe)
+
+def test_Pjointe_normal():
+    n1 = np.array(normale(21,1)[0])
+    n2 = np.array(normale(21,1)[0])
+    Pjointe = Pxy(n1,n2)
+    dessine(Pjointe)
     
 def main():
     #histogramme_binomiale(20)
     #plot_normale(1001,1)
     #plot_affine(21,0.001)
-    #PA = np.array(normale(21,1)[0])
-    #print(PA)
-    #PB = np.array(proba_affine(21,0.00001)[0])
-    #print(PB)
-    #Pjointe = Pxy(PA, PB)
-    #PA = np.array ( [0.2, 0.7, 0.1] )
-    #PB = np.array ( [0.4, 0.4, 0.2] )
-    #Pjointe = produit_tensoriel(PA, PB)
-    #print(Pjointe)
-    n = np.array(normale(21,1)[0])
-    print(n)
-    lin = np.array(proba_affine(21,0.000001)[0])
-    Pjointe = produit_tensoriel(n, lin)
-    #print(Pjointe)
+    #independance_conditionnelles()
+    gum_part()
     
-    # -----------------------
-    # La fonction dessine fonctionne c'est Pxy qui ne marche pas
-    # ----------------------
-    dessine(Pjointe)
 
 main()
